7bf913d0f95ae6ad194497940b5413759fd0889e
Jason Johnson <jasjoh@live.com>
2023-07-21 00:06:15 -0700

    new game api tests done

--- test_app.py
+++ test_app.py
@@ -36,12 +36,47 @@ class BoggleAppTestCase(TestCase):
         """Test starting a new game."""
 
         with self.client as client:
-            ...
             # write a test for this route
+            # check if game is a game
+            # check if board is filled
             response = client.post("/api/new-game")
-            json = response.get_data(as_text=True)
-            print("json is here:", json)
             response_dict = response.get_json()
+            board_length = len(response_dict.get("board"))
+            print("games:", games)
+
+            # check if board exists and is an iterable with a length > 0
+            self.assertTrue(board_length > 0)
+
+            # check that game ID is valid
+            self.assertTrue(games.get(response_dict.get("gameId")))
+
+    def test_api_score_word(self):
+        """Test scoring a word."""
+
+        with self.client as client:
+            # create a game
+            response = client.post("/api/new-game")
+            response_dict = response.get_json()
+
+            # get the game and overwrite the board
+            game_id = response_dict.get("gameId")
+            our_game = games[game_id]
+            print("our game board:", our_game.board)
+            # this has to be 3 x 3 (1 x 3 doesn't work)
+            our_game.board = [['C', 'A', 'T'],['C', 'A', 'T'],['C', 'A', 'T']]
+
+            # call score_word with CAT and get {result: "ok"}
+            # format of POST to endpoint expecting JSON is 'json={}', not 'data={}'
+            response = client.post("/api/score-word", json={"game_id": game_id, "word": "CAT"})
+            print("score word response", response)
+            response_dict = response.get_json()
+            print("score word response dict:", response_dict)
+
+
+            # call score_word with FOOBAR and get {result: "not-word"}
+            # call score_word with DOG and get {result: "not-on-board"}
+
+            ...
 
 
 
